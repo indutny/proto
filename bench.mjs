c3abf3a438771cb03a01c3b46f3d2d9b8061bf31
Fedor Indutny <238531+indutny@users.noreply.github.com>
2026-01-25 11:47:43 -0800

    save

--- bench.mjs
+++ bench.mjs
@@ -1,20 +1,12 @@
 import { parse } from './lib/index.mjs';
 
-const buf = new Uint8Array([
-  0x01,
-  0x96, 0x01,
-  0x96, 0x80, 0x01,
-  0x96, 0x80, 0x85, 0x01,
-  0x96, 0x80, 0x85, 0x82, 0x03,
-  0x01,
-  0x96, 0x01,
-  0x96, 0x80, 0x01,
-  0x96, 0x80, 0x85, 0x01,
-  0x96, 0x80, 0x85, 0x82, 0x03,
-]);
+const buf = Buffer.from(
+  '0a03474554120b2f76312f6d6573736167651a0568656c6c6f207b2a0e636f6e74656e742d6c656e6774682a04313032342a0c636f6e74656e742d747970652a0870726f746f627566',
+  'hex',
+);
 
 console.time('parse');
-for (let i = 0; i < 1e6; i++) {
+for (let i = 0; i < 2e6; i++) {
   parse(buf);
 }
 console.timeEnd('parse');
