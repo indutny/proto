217db68650750d12feb5d64e91f55e126329abff
Fedor Indutny <238531+indutny@users.noreply.github.com>
2026-01-27 08:26:44 -0800

    save

--- bench.mjs
+++ bench.mjs
@@ -1,10 +1,10 @@
 import {
-  parse,
+  decode,
   FIELD_HOLE,
   FIELD_STRING,
   FIELD_BYTES,
   FIELD_UINT64,
-} from './lib/index.mjs';
+} from './lib/decoder.mjs';
 
 const buf = Buffer.from(
   '0a03474554120b2f76312f6d6573736167651a0568656c6c6f207b2a0e636f6e74656e742d6c656e6774682a04313032342a0c636f6e74656e742d747970652a0870726f746f627566',
@@ -20,8 +20,8 @@ const MESSAGE_SPEC = [
   FIELD_STRING, // 5 - headers
 ];
 
-function decode(buf) {
-  const fields = parse(buf, MESSAGE_SPEC);
+function decodeMessage(buf) {
+  const fields = decode(buf, MESSAGE_SPEC);
 
   const res = {
     verb: null,
@@ -53,10 +53,10 @@ function decode(buf) {
   return res;
 }
 
-console.time('parse');
+console.time('decode');
 for (let i = 0; i < 2e6; i++) {
-  decode(buf);
+  decodeMessage(buf);
 }
-console.timeEnd('parse');
+console.timeEnd('decode');
 
-console.log(decode(buf));
+console.log(decodeMessage(buf));
